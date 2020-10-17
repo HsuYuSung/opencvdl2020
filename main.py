c5eb8d971aed4c5ad59ca06dde5e3f78d7188fff
joe-U16 <tand23744@gmail.com>
2020-10-17 17:05:31 +0800

    Try implement class mainwindow

--- main.py
+++ main.py
@@ -2,18 +2,40 @@
 import sys
 import matplotlib.pyplot as plt
 from main_ui import Ui_Opencvdl_HW1
-from PyQt5.QtWidgets import QApplication, QDockWidget, QPushButton
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+
+
+class mainwindow(QMainWindow ,Ui_Opencvdl_HW1):
+    def __init__(self):
+        super(mainwindow, self).__init__()
+
+    def setupUi(self, Ui_Opencvdl_HW1):
+        self.on_binding_ui()
+        
+    
+    def on_binding_ui(self):
+        self.pushButton.clicked.connect(self.load_image)
+
+    def load_image(self):
+        img = plt.imread('Uncle_Roger.jpg')
+        plt.imshow(img)
+        plt.show()
+        
+
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    new_ui = Ui_Opencvdl_HW1()
+    application = mainwindow()
     widget = QDockWidget()
-    new_ui.setupUi(widget)
-
-    ui = Ui_Opencvdl_HW1()
-    widget.show()
-    ex = new_ui
+    application.setupUi(widget)
+    # ui = Ui_Opencvdl_HW1()
+    # widget = QDockWidget()
+    # ui.setupUi(widget)
+    # widget.show()
+    # u
+    ex = application
     sys.exit(app.exec_())
