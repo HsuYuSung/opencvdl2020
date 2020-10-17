9a1468b77fe3c03d3a67cb02381fdc45ba0f8c8a
joe-U16 <tand23744@gmail.com>
2020-10-17 15:34:03 +0800

    Change the main.py
    main.py can work

--- qtttt.py
+++ qtttt.py
@@ -1,4 +1,4 @@
-from cv2 import cv2 as cv
+
 import sys
 import matplotlib.pyplot as plt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
--- test.py
+++ test.py
@@ -1,30 +1,24 @@
 from cv2 import cv2 as cv
 import sys
 import matplotlib.pyplot as plt
+from main_ui import Ui_Opencvdl_HW1
+from PyQt5.QtWidgets import QApplication, QDockWidget, QPushButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
 
 
-img = plt.imread("Flower.jpg").copy()
-b = img.copy()
-g = img.copy()
-r = img.copy()
-
-b[:,:,0] = 0.0
-b[:,:,1] = 0.0
-plt.figure('b')
-plt.imshow(b)
-
-g[:,:,0] = 0.0
-g[:,:,2] = 0.0
-plt.figure('g')
-plt.imshow(g)
+# class App(Ui_Opencvdl_HW1):
+#     def setupUi(self, Opencvdl_HW1):
+#         super().setupUi(Opencvdl_HW1)
+#     def retranslateUi(self, Opencvdl_HW1):
+#         super().retranslateUi(Opencvdl_HW1)
+    
 
 
-r[:,:,1] = 0.0
-r[:,:,2] = 0.0
-plt.figure('r')
-plt.imshow(r)
-
-
-
-
-plt.show()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ui = Ui_Opencvdl_HW1()
+    widget = QDockWidget()
+    ui.setupUi(widget)
+    
+    sys.exit(app.exec_())
