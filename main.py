877b0c7db0d6654fc1410ed29d674978ac64bb2c
joe-U16 <tand23744@gmail.com>
2020-10-17 21:45:06 +0800

    Done the button2

--- main.py
+++ main.py
@@ -17,10 +17,33 @@ class window(QDockWidget ,Ui_Opencvdl_HW1):
     
     def on_binding_ui(self):
         self.pushButton.clicked.connect(self.load_image)
+        self.pushButton_2.clicked.connect(self.Color_Separation)
 
     def load_image(self):
         img = plt.imread('Uncle_Roger.jpg')
         plt.imshow(img)
+        plt.show()
+
+    def Color_Separation(self):
+        img = plt.imread('Flower.jpg').copy()
+        r = img.copy()
+        g = img.copy()
+        b = img.copy()
+
+        r[:,:,1] = 0
+        r[:,:,2] = 0
+        g[:,:,0] = 0
+        g[:,:,2] = 0
+        b[:,:,0] = 0
+        b[:,:,1] = 0
+        plt.figure("origin")
+        plt.imshow(img)
+        plt.figure("r")
+        plt.imshow(r)
+        plt.figure("g")
+        plt.imshow(g)
+        plt.figure("b")
+        plt.imshow(b)
         plt.show()
         
 
