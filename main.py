c3d33c71ade6adf09934ffa78d68baedd1801091
joe-U16 <tand23744@gmail.com>
2020-10-17 21:10:10 +0800

    Implement pushbutton and pushbutton_1
    the load_image and color_seperate is well work

--- main.py
+++ main.py
@@ -3,39 +3,49 @@ import sys
 import matplotlib.pyplot as plt
 from main_ui import Ui_Opencvdl_HW1
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QDockWidget
 
 
 
+def load_image():
+    img = plt.imread("Uncle_Roger.jpg")
+    plt.imshow(img)
+    plt.show()
 
-class mainwindow(QMainWindow ,Ui_Opencvdl_HW1):
-    def __init__(self):
-        super(mainwindow, self).__init__()
+def Color_Separation():
+    img = plt.imread('Flower.jpg').copy()
+    r = img.copy()
+    g = img.copy()
+    b = img.copy()
 
-    def setupUi(self, Ui_Opencvdl_HW1):
-        self.on_binding_ui()
-        
-    
-    def on_binding_ui(self):
-        self.pushButton.clicked.connect(self.load_image)
+    r[:,:,1] = 0
+    r[:,:,2] = 0
+    g[:,:,0] = 0
+    g[:,:,2] = 0
+    b[:,:,0] = 0
+    b[:,:,1] = 0
+    plt.figure("origin")
+    plt.imshow(img)
+    plt.figure("r")
+    plt.imshow(r)
+    plt.figure("g")
+    plt.imshow(g)
+    plt.figure("b")
+    plt.imshow(b)
+    plt.show()
 
-    def load_image(self):
-        img = plt.imread('Uncle_Roger.jpg')
-        plt.imshow(img)
-        plt.show()
-        
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    application = mainwindow()
     widget = QDockWidget()
-    application.setupUi(widget)
-    # ui = Ui_Opencvdl_HW1()
-    # widget = QDockWidget()
-    # ui.setupUi(widget)
-    # widget.show()
-    # u
-    ex = application
+    #test
+    ui = Ui_Opencvdl_HW1()
+    ui.setupUi(widget)
+    
+    ui.pushButton.clicked.connect(load_image)
+    ui.pushButton_2.clicked.connect(Color_Separation)
+
+    widget.show()
+    ex = ui
+
     sys.exit(app.exec_())
