db195fc6979035810157ade3069c5572d622d1b8
joe-U16 <tand23744@gmail.com>
2020-10-18 11:32:36 +0800

    Implement median gaussian bilateral filter image

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 
 import sys
+from cv2 import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.widgets import Slider, Button, RadioButtons
 from main_ui import Ui_Opencvdl_HW1
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDockWidget
-import numpy as np
-from matplotlib.widgets import Slider, Button, RadioButtons
 
 
 
@@ -24,6 +23,9 @@ class window(QDockWidget ,Ui_Opencvdl_HW1):
         self.pushButton_2.clicked.connect(self.Color_Separation)
         self.pushButton_3.clicked.connect(self.Image_Flipping)
         self.pushButton_4.clicked.connect(self.Image_Tracebar)
+        self.pushButton_5.clicked.connect(self.Median_filter)
+        self.pushButton_6.clicked.connect(self.Gaussian_filter)
+        self.pushButton_7.clicked.connect(self.Bilateral_filter)
 
     def load_image(self):
         img = plt.imread('Uncle_Roger.jpg')
@@ -73,20 +75,41 @@ class window(QDockWidget ,Ui_Opencvdl_HW1):
         plt.imshow(img_reverse, alpha = 0.5)
         plt.imshow(img, alpha = 0.5)
 
-        def image(val):
-            plt.cla()
-            plt.imshow(img_reverse, alpha = 1 - val)
-            plt.imshow(img, alpha = val)
-            plt.show()
-            
-        def update(val):
-            value = sbar.val
-            image(value)
-
-        sbar.on_changed(update)
+    def Median_filter(self):
+        img = cv.imread('Cat.png')
+        median = cv.medianBlur(img, 7)
+        plt_img = img [:,:,::-1]
+        plt.figure('origin')
+        plt.imshow(plt_img)
+        plt_median = median[:,:,::-1]
+        plt.figure('median')
+        plt.imshow(plt_median)
         plt.show()
 
-        
+    def Gaussian_filter(self):
+        img = cv.imread('Cat.png')
+        gaussian_img = cv.GaussianBlur(img,(3,3),0)
+        plt_img = img [:,:,::-1]
+        plt.figure('origin')
+        plt.imshow(plt_img)
+        plt_gaussian_img = gaussian_img[:,:,::-1]
+        plt.figure('Gaussian')
+        plt.imshow(plt_gaussian_img)
+        plt.show()
+
+    def Bilateral_filter(self):
+        img = cv.imread('Cat.png')
+        blur = cv.bilateralFilter(img,9,90,90)
+
+        plt_img = img [:,:,::-1]
+        plt.figure('origin')
+        plt.imshow(plt_img)
+        plt_blur = blur[:,:,::-1]
+        plt.figure('Bilateral')
+        plt.imshow(plt_blur)
+        plt.show()
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
